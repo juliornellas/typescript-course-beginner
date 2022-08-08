725c56024bdfbf649b39c8263beec5d581a72e55
Julio <juliornellas@gmail.com>
2022-08-08 15:59:00 -0300

    First class, incomplete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-const a = "1";
+/**
+ * PRACTICE
+ * CLASS 1
+ * Explanations about : CONST, LET and VAR
+ */
+
+//CONST. Can't change to "hello: string" because CONST you cannot change at all. So if you pass mouse over "hello" you'll' see "world" as a type. Like was a specific type
+const hello = "world";
+// hello = "planet";
+
+//LET. You can assign the types. Pass mouse over "world", now, the type is "string"
+let world = "hello";
